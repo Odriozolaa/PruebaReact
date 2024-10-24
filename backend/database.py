ca27c59ffd34c571e42ae1d22b34c69099fd6dd7
Odriozolaa <odriozolaa04@gmail.com>
2024-10-24 00:26:01 -0600

    url base de datos

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@db:5432/mydatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://josuejo17:Jbrasil#174.@pruebadb.c1gjok2a0plusa-us-east-2.rds.amazonaws.com:5432/myappdb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
