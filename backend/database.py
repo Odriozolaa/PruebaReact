53f03e4612f1fe4e5d593799dac8fb1f4425ba69
Odriozolaa <odriozolaa04@gmail.com>
2024-10-28 14:12:17 -0600

    regreso a base de datos local generica y a docker para que funcione local

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://josuejo17:Jbrasil#174.@pruebadb.c1gi02ka0plo.us-east-2.rds.amazonaws.com:5432/myappdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://myuser:mypassword@db:5432/mydatabase"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
